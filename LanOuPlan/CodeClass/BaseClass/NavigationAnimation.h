2c310806833fb4e1da7fdb27f1b5d40d5d9957b0
XiuFi <616807404@qq.com>
2018-09-30 17:54:08 +0800

    删除了storyboard 实现了自定义转场一部分代码，回去继续撸

---
+++ LanOuPlan/CodeClass/BaseClass/NavigationAnimation.h
@@ -0,0 +1,17 @@
+//
+//  NavigationAnimation.h
+//  LanOuPlan
+//
+//  Created by monda on 2018/9/30.
+//  Copyright © 2018 lzy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NavigationAnimation : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LanOuPlan/CodeClass/BaseClass/NavigationAnimation.m
@@ -0,0 +1,13 @@
+//
+//  NavigationAnimation.m
+//  LanOuPlan
+//
+//  Created by monda on 2018/9/30.
+//  Copyright © 2018 lzy. All rights reserved.
+//
+
+#import "NavigationAnimation.h"
+
+@implementation NavigationAnimation
+
+@end
